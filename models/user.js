3f1d86607d97b4387cb595985e90f7145d1227c0
Joseph Shull <jshull@neumont.edu>
2023-08-24 10:20:24 -0600

    add user model

---
+++ models/user.js
@@ -0,0 +1,11 @@
+module.exports = class User {
+    constructor(username, password, email) {
+        this.username = username
+        this.password = password
+        this.email = email
+    }
+
+    createUser() {
+        console.log("we have access to this!!")
+    }
+}
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var userModel = require('../models/user')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.post('/', function(req, res) {
+  try {
+    let user = req.body
+    userModel.createUser()
+    res.sendStatus(200)
+  }
+  catch (err) {
+    res.sendStatus(500)
+  }
+})
 
 module.exports = router;
